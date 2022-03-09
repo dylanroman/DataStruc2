f3d52eceaed4743e4ecc0520e6fa007b0f205f3d
dylanroman <dylanroman143@gmail.com>
2022-03-09 09:19:32 -0800

    added dictionary/hashmap and class

--- src/main.java
+++ src/main.java
@@ -1,2 +1,14 @@
+import java.util.Dictionary;
+import java.util.Hashtable;
+
 public class main {
+
+    public static void main(String[] args) {
+        Dictionary<Integer, String> myElements = new Hashtable<Integer, String>(); {
+            myElements.put(1, "Test_1");
+            myElements.put(2, "Test_2");
+        }
+
+        menu myMenu = new menu(myElements);
+    }
 }
---
+++ src/menu.java
@@ -0,0 +1,12 @@
+import java.util.Dictionary;
+
+public class menu {
+
+    private Dictionary elements;
+
+    public menu (Dictionary elements) {
+        this.elements = elements;
+    }
+
+
+}
