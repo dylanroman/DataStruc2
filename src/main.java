5edfacffa335ca65c5680a5018f8c14a63dfc6dc
dylanroman <dylanroman143@gmail.com>
2022-03-09 21:48:00 -0800

    added to menu class

--- src/main.java
+++ src/main.java
@@ -1,14 +1,28 @@
+//imports
 import java.util.Dictionary;
 import java.util.Hashtable;
+import java.util.Scanner;
 
 public class main {
 
     public static void main(String[] args) {
+        //Initialize scanner (user input)
+        Scanner input = new Scanner(System.in);
+
+        //Create menu options dictionary
         Dictionary<Integer, String> myElements = new Hashtable<Integer, String>(); {
             myElements.put(1, "Test_1");
             myElements.put(2, "Test_2");
         }
-
+        
+        //Create menu class object
         menu myMenu = new menu(myElements);
+
+        //Print insturctions
+        System.out.println("Enter Item Number From Following List: ");
+        myMenu.print();
+
+        //Read user input
+        int choice = input.nextInt();
     }
 }
--- src/menu.java
+++ src/menu.java
@@ -2,10 +2,21 @@ import java.util.Dictionary;
 
 public class menu {
 
+    //Initialize variables
     private Dictionary elements;
 
+    //Constructor
     public menu (Dictionary elements) {
+        //Takes dictionary as input
         this.elements = elements;
+    }
+
+    //Iterate over dictionary and print all values
+    public void print() {
+        for(int i = 1; i <= this.elements.size(); i++) {
+            System.out.print(i + " ");
+            System.out.println(elements.get(i));
+        }
     }
 
 
